f76d1703e6aad58accc29f517cafe10dd434723a
Johannes Sebastian Lade <johannes.lade@yahoo.com>
2017-11-12 14:27:36 +0100

    Changed signature of IO static methods.

--- src/utils/IO.java
+++ src/utils/IO.java
@@ -11,43 +11,42 @@ public class IO {
     public static <T> void print(T[] array) { System.out.println(toString(array));}
 
 
-    private static <T> String toString(T[] array) {
-        return toString(array, 0, array.length-1);
+    public static <T> String toString(T[] array) {
+        return toString(array, 0, array.length);
     }
 
-    private static <T> String toString(T[] array, int start, int end) {
+    public static <T> String toString(T[] array, int start, int end) {
         StringBuilder sb = new StringBuilder();
         sb.append('[');
-        for (int i=start; i<end; i++) {
+        for (int i = start; i < end-1; i++) {
             sb.append(array[i]).append(",\n");
         }
         sb.append(array[end]).append("]");
         return sb.toString();
     }
 
-
     public static String toString(double[] array) {
-        return toString(array, 0, array.length-1);
+        return toString(array, 0, array.length);
     }
 
-    private static String toString(double[] a, int start, int end) {
+    public static String toString(double[] a, int start, int end) {
         StringBuilder sb = new StringBuilder();
         sb.append('[');
-        for (int i=start; i<end; i++) {
+        for (int i = start; i < end - 1; i++) {
             sb.append(a[i]).append(", ");
         }
         sb.append(a[end]).append("]");
         return sb.toString();
     }
 
-    private static String toString(int[] array) {
-        return toString(array, 0, array.length-1);
+    public static String toString(int[] array) {
+        return toString(array, 0, array.length);
     }
 
-    private static String toString(int[] a, int start, int end) {
+    public static String toString(int[] a, int start, int end) {
         StringBuilder sb = new StringBuilder();
         sb.append('[');
-        for (int i=start; i<end; i++) {
+        for (int i = start; i < end - 1; i++) {
             sb.append(a[i]).append(", ");
         }
         sb.append(a[end]).append("]");
